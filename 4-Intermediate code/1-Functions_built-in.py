b613df96586eaa20717643d7220bec30928a0206
lxweb102 <lxweb102@gmail.com>
2023-07-03 20:21:26 -0300

    Adding more built-in functions

--- 4-Intermediate code/1-Functions_built-in.py
+++ 4-Intermediate code/1-Functions_built-in.py
@@ -12,6 +12,28 @@ print( dir(my_var) )
 """
 List methods
 """
+numbers = [-3, 0, 5, 7, 100, 8.9, -8]
+
+maximum_number = max(numbers)
+
+print(maximum_number)
+
+minimum_number = min(numbers)
+
+print(minimum_number)
+
+print( round(999.999) )
+print( round(999.999, 1) )
+print( round(999.999, 2) )
+print( round(999.999, 3) )
 
 
+# The all function test all the items in an iterable and return True only if every item is Truly
+print(all([True, "yes", 0]))
+print(all([True, "yes", 1]))
 
+# sum all the items - Needs to be numeric - Receives an iterable
+
+print(sum([3, 3, 3]))
+print(sum([3, 3.78]))
+print(sum([3, -5]))
