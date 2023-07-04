4b50228bc906b8cf05555aadcfa454a22b4444ad
lxweb102 <lxweb102@gmail.com>
2023-07-04 02:40:46 -0300

    Adding positional arguments and keyword parameters

--- 5-Advanced code/1-Functions.py
+++ 5-Advanced code/1-Functions.py
@@ -29,3 +29,27 @@ def return_a_random_number():
 my_random_var = return_a_random_number()
 
 print(my_random_var)
+
+def return_a_tuple():
+    value_1 = True
+    value_2 = 2
+    value_3 = 'returning a tuple in my function'
+    return (value_1, value_2, value_3)
+
+my_var_1, my_var_2, my_var_3 = return_a_tuple()
+
+print(my_var_1, my_var_2, my_var_3)
+
+# getting all the arguments
+def sum_all_args(*numbers):
+    return sum(numbers)
+
+my_sum = sum_all_args(1,2,3,4,-9,-10)
+print(my_sum)
+
+# Using arguments by name 
+def greet_with_arguments(first_name, last_name, age):
+    return f'Hello dear {first_name} {last_name}, you are {age} years old'
+
+print(greet_with_arguments('Mike', 'Smith', 50))
+print(greet_with_arguments(age=40, first_name='Josh', last_name='Hilton'))
