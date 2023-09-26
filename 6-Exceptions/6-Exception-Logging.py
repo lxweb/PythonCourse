e3ac6777714cd19d7eccd47c0d447b9fa32a248c
lxweb102 <lxweb102@gmail.com>
2023-09-26 17:14:37 -0300

    Logging exceptions

---
+++ 6-Exceptions/6-Exception-Logging.py
@@ -0,0 +1,18 @@
+import logging
+dividers = [1,3,6,7,9,0,3,5,2,7,5, "hello"]
+
+# We will devide 10 for each devicder and see if there is an error
+for value in dividers:
+    try:
+        print(10/value)
+        if value == 9:
+            print( int("Testing exceptions") ) # This line rise a ValueError 
+    except ZeroDivisionError as zde:
+        print("Zero division exception: on 10 / ", str(value))
+    except ValueError as ve:
+        print("Value exception: ", str(ve))
+    except Exception as e:
+        logging.exception(e)
+    
+print("The exception was handled succesfuly")
+    
